537d223d40d271b8b7ce0f1adb10e8ee487fb439
NiklausRupail <m.rup.business@gmail.com>
2023-05-31 01:54:08 +0200

    Fixed TS issues

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -7,16 +7,16 @@ const Contact = () => {
     <div className={styles.contact}>
       <div className={styles.phoneNums}>
         <a href='tel:500-500-500'>
-          <PhoneIcon />
+          <PhoneIcon fillColor={undefined} />
           <p>+48 500 500 500</p>
         </a>
         <a href='tel:323-332-222'>
-          <PhoneIcon />
+          <PhoneIcon fillColor={undefined} />
           <p>32 333 22 22</p>
         </a>
       </div>
       <a href='mailto:adriannowak@wp.pl' className={styles.mail}>
-        <MailIcon />
+        <MailIcon fillColor={undefined} />
         <p>adriannowak@wp.pl</p>
       </a>
     </div>
